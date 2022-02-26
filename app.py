10fc08dd12079c7e74ac85eb5c0b86d51f377f63
Elliot Redhead <elliot.redhead@gmail.com>
2022-02-26 01:05:02 +0000

    Add standalone HTML routing example

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route("/")
 def hello_world():
     """Basic view to check Flask routing"""
     return "Hello world"
+
+
+@app.route("/standalone")
+def basic_html():
+    """Display a single HTML file only"""
+    return render_template("pages/standalone.html")
 
 
 if __name__ == "__main__":
