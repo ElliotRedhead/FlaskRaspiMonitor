37ad2b944fc325302e93b95ea5b24d4bddc7108f
Elliot Redhead <elliot.redhead@gmail.com>
2022-02-26 01:12:24 +0000

    Add "stitched" HTML example with Context Processor

--- app.py
+++ app.py
@@ -16,5 +16,13 @@ def basic_html():
     return render_template("pages/standalone.html")
 
 
+@app.route("/basic-include")
+def stitched_html():
+    """Example of Jinja include, and tab title determined using Context Processor"""
+    # https://jinja.palletsprojects.com/en/3.0.x/templates/#include
+    # https://flask.palletsprojects.com/en/2.0.x/templating/#context-processors
+    return render_template("structure/base.html", title="Injected title")
+
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=app.debug)
